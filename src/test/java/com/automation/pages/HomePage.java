782f3e6afa14a46f0083fd9e2dd6684a13bdb89f
Mustafa Korkmaz <118779406+mkorkmaz71@users.noreply.github.com>
2023-06-08 00:02:48 -0500

    initial commit

---
+++ src/test/java/com/automation/pages/HomePage.java
@@ -0,0 +1,44 @@
+package com.automation.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.junit.Assert;
+
+public class HomePage extends BasePage {
+
+    @FindBy(xpath = "//button[contains(@id,'add-to-cart')]")
+    WebElement item1;
+
+    @FindBy(xpath = "//a[@class = 'shopping_cart_link']")
+    WebElement cart;
+
+    @FindBy(xpath = "//select[@class='product_sort_container']")
+    WebElement filter;
+
+    @FindBy(id = "react-burger-menu-btn")
+    WebElement burgerIcon;
+
+    @FindBy(id = "logout_sidebar_link")
+    WebElement logoutLink;
+
+    public void clickOnAddToCartBtnOfFirstProduct() {
+        item1.click();
+    }
+
+    public void clickOnCartIcon() {
+        cart.click();
+    }
+
+    public void verifyHomePage() {
+        Assert.assertTrue(cart.isDisplayed());
+        Assert.assertTrue(filter.isDisplayed());
+    }
+
+    public void clickOnHamburgerIcon() {
+        burgerIcon.click();
+    }
+
+    public void clickOnLogoutLink() {
+        logoutLink.click();
+    }
+}
